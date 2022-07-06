8e54acd0b573e27294a70fd7c80e35f8c4598eef
priya1503 <blpriya1503@gmail.com>
2022-07-06 15:29:00 +0530

    updated url

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ export const HomePage = () =>{
     useEffect(
         () => {
             const fetchAllTeams = async () =>{
-                const response = await fetch(`process.env.REACT_APP_API_ROOT_URL/team`);
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team`);
                 const data = await response.json();
                 setTeams(data);
             };
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -11,7 +11,7 @@ export const TeamPage = () => {
     useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch(`process.env.REACT_APP_API_ROOT_URL/team/${teamName}`);
+                const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
             };
